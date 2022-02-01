e06d7647a509c49e307947d6753c6e6cce21760e
poonam09aug <37356537+poonam09aug@users.noreply.github.com>
2022-01-31 18:41:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-https://teachablemachine.withgoogle.com/models/qEFbSJkb7/
+
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/qEFbSJkb7/model.json', modelReady);
+}
+
+modelReady()
+{
+    classifier.classify(gotResults);
+}
